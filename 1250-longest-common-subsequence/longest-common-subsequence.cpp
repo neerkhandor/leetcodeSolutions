42768f5e3234250e64b67b75177c835753376677
neerkhandor <82304249+neerkhandor@users.noreply.github.com>
2023-09-01 10:54:57 +0530

    Time: 57 ms (53.12%) | Memory: 24.5 MB (51.22%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int solveUsingRecursion(string text1,string text2,int i,int j){
+        //agar pehli string khatam hogayi then 0 return kardo
+        if(i==text1.length())
+            return 0;
+        if(j==text2.length())
+            return 0;
+        if(text1[i]==text2[j]){
+            return 1+solveUsingRecursion(text1,text2,i+1,j+1);
+        }
+        else{
+            return max(solveUsingRecursion(text1,text2,i+1,j),solveUsingRecursion(text1,text2,i,j+1));
+        }
+    }
+     int solveUsingMem(string &text1,string &text2,int i,int j,vector<vector<int>>&dp){
+        //agar pehli string khatam hogayi then 0 return kardo
+        if(i==text1.length())
+            return 0;
+        if(j==text2.length())
+            return 0;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(text1[i]==text2[j]){
+            dp[i][j]= 1+solveUsingMem(text1,text2,i+1,j+1,dp);
+            return dp[i][j];
+        }
+        else{
+            dp[i][j]= max(solveUsingMem(text1,text2,i+1,j,dp),solveUsingMem(text1,text2,i,j+1,dp));
+            return dp[i][j];
+        }
+    }
+    int solveUsingTab(string text1,string text2){
+        vector<vector<int> >dp(text1.length()+1,vector<int>(text2.length()+1,0));
+        //here we dont need base case as our base case is already handled since we are initiallizing with 0
+        //text 1 is going from 0 to text1.length and similarly text2 is also going from 0 to text2.length
+        //so for tabulation sol we will do reverse
+        
+        for(int i=text1.length()-1;i>=0;i--){
+            for(int j=text2.length()-1;j>=0;j--){
+            int ans=0;
+                 if(text1[i]==text2[j]){
+                 ans= 1+dp[i+1][j+1];
+             }
+             else{
+               ans= max(dp[i+1][j],dp[i][j+1]);
+               }
+               dp[i][j]=ans;
+            }
+        }
+
+    return dp[0][0];
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int i=0;
+        int j=0;
+        // return solveUsingRecursion(text1,text2,i,j);   
+        // vector<vector<int> >dp(text1.length(),vector<int>(text2.length(),-1));
+        // return solveUsingMem(text1,text2,i,j,dp);
+        return solveUsingTab(text1,text2);
+
+    }
+};
