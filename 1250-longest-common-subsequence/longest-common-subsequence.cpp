a9f3c44852d776405aae03b00d1dbc5e12f5b1ce
neerkhandor <82304249+neerkhandor@users.noreply.github.com>
2023-09-01 11:09:40 +0530

    Time: 32 ms (87.72%) | Memory: 24.6 MB (29.29%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -22,14 +22,16 @@ public:
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
+        int ans=0;
         if(text1[i]==text2[j]){
-            dp[i][j]= 1+solveUsingMem(text1,text2,i+1,j+1,dp);
-            return dp[i][j];
+            ans= 1+solveUsingMem(text1,text2,i+1,j+1,dp);
+     
         }
         else{
-            dp[i][j]= max(solveUsingMem(text1,text2,i+1,j,dp),solveUsingMem(text1,text2,i,j+1,dp));
-            return dp[i][j];
+            ans= max(solveUsingMem(text1,text2,i+1,j,dp),solveUsingMem(text1,text2,i,j+1,dp));   
         }
+        dp[i][j]=ans;
+        return dp[i][j];
     }
     int solveUsingTab(string text1,string text2){
         vector<vector<int> >dp(text1.length()+1,vector<int>(text2.length()+1,0));
@@ -52,13 +54,37 @@ public:
 
     return dp[0][0];
     }
+    int solveUsingSpaceOptimized(string &text1,string &text2){
+        vector<int>curr(text2.length()+2,0);
+        vector<int>next(text2.length()+2,0);
+         for(int i=text1.length()-1;i>=0;i--){
+            for(int j=text2.length()-1;j>=0;j--){
+            int ans=0;
+                 if(text1[i]==text2[j]){
+                 ans= 1+next[j+1];
+             }
+             else{
+               ans= max(next[j],curr[j+1]);
+               }
+               curr[j]=ans;
+            }
+            //mein yehi pakka bhulunga
+            //shifting 
+            //since ham ulta ja raha hai
+            next=curr;
+        }
+
+    return next[0];
+
+    }
     int longestCommonSubsequence(string text1, string text2) {
         int i=0;
         int j=0;
         // return solveUsingRecursion(text1,text2,i,j);   
-        // vector<vector<int> >dp(text1.length(),vector<int>(text2.length(),-1));
+         vector<vector<int> >dp(text1.length(),vector<int>(text2.length(),-1));
         // return solveUsingMem(text1,text2,i,j,dp);
-        return solveUsingTab(text1,text2);
+        //return solveUsingTab(text1,text2);
+        return solveUsingSpaceOptimized(text1,text2);
 
     }
 };
