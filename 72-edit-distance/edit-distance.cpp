f110368b08c6ec9ceae0c637cf913d988665d39b
neerkhandor <82304249+neerkhandor@users.noreply.github.com>
2023-09-01 13:04:35 +0530

    Time: 7 ms (88.15%) | Memory: 9.1 MB (27.22%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -20,6 +20,7 @@ public:
         return ans;
     }
     int solveUsingMem(string word1,string word2,int i,int j,vector<vector<int>>&dp){
+        
          if(i==word1.length()){
             return word2.length()-j;
         }
@@ -42,6 +43,36 @@ public:
         dp[i][j]=ans;
         return dp[i][j];
     }
+    int SolveUsingTab(string word1,string word2){
+        //step1 create dp array
+        //step2 analyze base case
+        //agar hamara i == a.length() ke barabar ho matlab ham i+1 index ko access karne ki koshish kar 
+        //rahe hai so in the size of dp array do +1
+        //agar mujhe a.length() access karna hai to mere array ka size atleast a.length()+1 to hona chahiye
+        vector<vector<int>> dp(word1.length()+1,vector<int>(word2.length()+1,0));
+        for(int j=0;j<word2.length();j++){
+            dp[word1.length()][j]=word2.length()-j;
+        }
+        for(int i=0;i<word1.length();i++){
+            dp[i][word2.length()]=word1.length()-i;
+        }
+        for(int i=word1.length()-1;i>=0;i--){
+            for(int j=word2.length()-1;j>=0;j--){
+                int ans=0;
+             if(word1[i]==word2[j]){
+             ans=dp[i+1][j+1];
+        }
+        else{
+            int insert=1+dp[i][j+1];
+            int replace=1+dp[i+1][j+1];
+            int deleted=1+dp[i+1][j];
+            ans=min(insert,min(replace,deleted));
+                }
+            dp[i][j]=ans;
+            }
+        }
+    return dp[0][0];
+    }
     int minDistance(string word1, string word2) {
         if(word1.length()==0){
             return word2.length();
@@ -51,7 +82,8 @@ public:
         }
         int i=0,j=0;
         //return solveUsingRec(word1,word2,i,j);
-        vector<vector<int>> dp(word1.length(),vector<int>(word2.length(),-1));
-        return solveUsingMem(word1,word2,i,j,dp);
+        // vector<vector<int>> dp(word1.length(),vector<int>(word2.length(),-1));
+        // return solveUsingMem(word1,word2,i,j,dp);
+        return SolveUsingTab(word1,word2);
     }
 };
