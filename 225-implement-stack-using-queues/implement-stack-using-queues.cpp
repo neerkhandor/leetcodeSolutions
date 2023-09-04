6a89968379f26e2c43b2cd64cfe3eb108e19d889
neerkhandor <82304249+neerkhandor@users.noreply.github.com>
2023-09-04 12:09:42 +0530

    Time: 3 ms (38.90%) | Memory: 6.8 MB (65.10%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,23 +1,38 @@
 class MyStack {
 public:
-queue<int>q1,q2;
+// queue<int>q1,q2;
+queue<int>q1;
     MyStack() {
         
     }
     
     void push(int x) {
-        q2.push(x);
-        while(!q1.empty()){
-            q2.push(q1.front());
+        // q2.push(x);
+        // while(!q1.empty()){
+        //     q2.push(q1.front());
+        //     q1.pop();
+        // }
+        // while(!q2.empty()){
+        //     q1.push(q2.front());
+        //     q2.pop();
+        // }
+
+        //m2
+        q1.push(x);
+        for(int i=0;i<q1.size()-1;i++){
+            int top=q1.front();
             q1.pop();
-        }
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
+            q1.push(top);
         }
     }
     
     int pop() {
+        // int popp=-1;
+        // popp=q1.front();
+        // q1.pop();
+        // return popp;
+
+        //m2
         int popp=-1;
         popp=q1.front();
         q1.pop();
@@ -25,13 +40,19 @@ queue<int>q1,q2;
     }
     
     int top() {
+        // int topp=-1;
+        // topp=q1.front();
+        // return topp;
+
+        //m2
         int topp=-1;
         topp=q1.front();
         return topp;
     }
     
     bool empty() {
-        return q1.empty() && q2.empty();
+        // return q1.empty() && q2.empty();
+        return q1.empty();
     }
 };
 
