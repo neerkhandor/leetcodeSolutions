7689939cf94fdde305631b1fb3eafdbaf888311e
neerkhandor <82304249+neerkhandor@users.noreply.github.com>
2023-09-04 11:54:58 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (86.26%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -6,38 +6,59 @@ public:
     }
     
     void push(int x) {
+       // s1.push(x);
+       //m-2
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
         s1.push(x);
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
     }
     
     int pop() {
-        int popped=-1;
-        if(!s2.empty()){
-            popped=s2.top();
-        }
-        else{
-            while(!s1.empty()){
-                s2.push(s1.top());
-                s1.pop();
-            }
-            popped=s2.top();
-        }
-        s2.pop();
-        return popped;
+        // int popped=-1;
+        // if(!s2.empty()){
+        //     popped=s2.top();
+        // }
+        // else{
+        //     while(!s1.empty()){
+        //         s2.push(s1.top());
+        //         s1.pop();
+        //     }
+        //     popped=s2.top();
+        // }
+        // s2.pop();
+        // return popped;
+
+        //m2
+        int popp=-1;
+        popp=s1.top();
+        s1.pop();
+        return popp;
     }
     
     int peek() {
+        // int front=-1;
+        // if(!s2.empty()){
+        //     front=s2.top();
+        // }
+        // else{
+        //     while(!s1.empty()){
+        //         s2.push(s1.top());
+        //         s1.pop();
+        //     }
+        //     front=s2.top();
+        // }
+        // // s2.pop();
+        // return front;
+
+        //m2
         int front=-1;
-        if(!s2.empty()){
-            front=s2.top();
-        }
-        else{
-            while(!s1.empty()){
-                s2.push(s1.top());
-                s1.pop();
-            }
-            front=s2.top();
-        }
-        // s2.pop();
+        front=s1.top();
         return front;
     }
     
